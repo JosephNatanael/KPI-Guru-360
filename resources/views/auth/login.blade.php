1a98c86983d69abf7a7bf7cc84a7d58b29cc6e7a
Joseph Natanael <ocepmorasa@gmail.com>
2026-02-09 21:42:21 +0700

    commit 5.3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -357,7 +357,7 @@
                         <i class="fas fa-school"></i>
                     </div>
                     
-                    <h1 class="app-name">PENILAIAN KINERJA GURU KPI 360</h1>
+                    <h1 class="app-name">PENILAIAN KINERJA GURU</h1>
                     <h5 class="app-subtitle" style="color: #dfe5e9ff;">SEKOLAH KRISTEN PERMATA HATI MANADO</h5>
         
                 </div>
